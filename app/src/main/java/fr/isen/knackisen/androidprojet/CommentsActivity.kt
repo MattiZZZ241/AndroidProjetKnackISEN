e94cf6db1eaeadab878ef33d2bdbebfa5a9f5644
Tomitom13 <thomas.buton@isen.yncrea.Fr>
2023-01-30 15:31:53 +0100

    Read online comment

--- app/src/main/java/fr/isen/knackisen/androidprojet/CommentsActivity.kt
+++ app/src/main/java/fr/isen/knackisen/androidprojet/CommentsActivity.kt
@@ -7,10 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import fr.isen.knackisen.androidprojet.adapter.CommentsAdapter
@@ -21,54 +18,42 @@ import fr.isen.knackisen.androidprojet.databinding.ActivityLoginBinding
 
 class CommentsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCommentsBinding
-    private lateinit var database: DatabaseReference
+    private lateinit var database: FirebaseDatabase
     private lateinit var commentsAdapter: CommentsAdapter
+private lateinit var listComment: List<Comment>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCommentsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        listComment = listOf()
+        database = Firebase.database
 
-        database = Firebase.database.getReference("comments")
-
-        database.addValueEventListener(object: ValueEventListener {
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-               /* var listString: String = snapshot.getValue().toString()
-                listString.split("Comment").map {
-                    This is it
-                    * {-NN1qEMr1Z_MuSrl8V_Q={id=-NN1qEMr1Z_MuSrl8V_Q, user={name=name, id=1}, content=ddik}, -NN1rqloI9VJf0fxtIwq={id=-NN1rqloI9VJf0fxtIwq, user={name=name, id=1}, content=fht}, -NN1opE3W5ylgyMdTI7X={user={name=name, id=1}, content=fiju}, -NN1o5vqFTxpDkwB5EJY={user={name=name, id=1}, content=gguui}, -NN1sWmMJGzI-iABwXxn={id=-NN1sWmMJGzI-iABwXxn, user={name=name, id=1}, content=dhhn}, user={name=name, id=1}, content=mhhhhtncfiu}
-
-                    var id = it.substringAfter("id=").substringBefore(", user")
-                    var content = it.substringAfter("content=").substringBefore("}")
-                    var name = it.substringAfter("name=").substringBefore(", id")
+        database.reference.child("comments").get().addOnCompleteListener() { task ->
+            if (task.isSuccessful) {
+                listComment = listOf()
+                for (snapshot in task.result!!.children) {
+                    var id = snapshot.child("id").value.toString()
+                    var content = snapshot.child("content").value.toString()
+                    var name = snapshot.child("user").child("name").value.toString()
                     var user = User(1, name)
                     var comment = Comment(id, content, user)
-                    Log.i("TAG", "Value is: ${comment}")
-                    *
-                    // Fill a variable with the list of comments
-
+                    listComment += comment
                 }
-                var listList: List<Comment> = listOf()
-                commentsAdapter.updateList(listList)
-                Log.i("TAG", "Value is: ${snapshot.getValue()}")
+                Log.i("TAG", "Value is: ${listComment}")
+                commentsAdapter.updateList(listComment)
 
-                */
+            } else {
+                Log.d("VALUE", task.exception?.message.toString())
             }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
-            }
-        })
+        }
 
         var newComment = fun () {
             val i = Intent(this@CommentsActivity, AddCommentActivity::class.java)
             startActivity(i)
         }
-        var commentList = listOf(Comment("test","This is the message", User(1, "Serg")),Comment("id","Another message", User(2, "L'autre")))
-        commentsAdapter = CommentsAdapter(commentList)
+       // var commentList = listOf(Comment("test","This is the message", User(1, "Serg")),Comment("id","Another message", User(2, "L'autre")))
+        commentsAdapter = CommentsAdapter(listComment)
         binding.listComments.adapter = commentsAdapter
         binding.listComments.layoutManager = LinearLayoutManager(this)
         binding.newComment.setOnClickListener { newComment() }
