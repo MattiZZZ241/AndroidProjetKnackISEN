a76a4e1dcabfeb2e55d0ea85dfdfef72182e1d7a
Sorroche <loic.sorroche@isen.yncrea.fr>
2023-01-30 15:16:21 +0100

    il reste a écrire dans la bdd et faire des tests

---
+++ app/src/main/java/fr/isen/knackisen/androidprojet/PoubelActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.knackisen.androidprojet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PoubelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poubel)
+    }
+}
