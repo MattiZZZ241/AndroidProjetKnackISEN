e4a85c30cd149d51d3bc018c36826207521fa559
Bajiio <mathias.cousseau@isen.yncrea.fr>
2023-01-31 17:09:42 +0100

    gitflow-feature-stash: FixMergeFormSignUp

--- app/src/main/java/fr/isen/knackisen/androidprojet/data/model/UserInfo.kt
+++ app/src/main/java/fr/isen/knackisen/androidprojet/data/model/UserInfo.kt
@@ -1,3 +1,3 @@
 package fr.isen.knackisen.androidprojet.data.model
 
-data class UserInfo(var name: String="", var age: Int=0, var email: String="", var password: String="",var genre: String="", var description: String="", var image: String="", var uid: String="")
+data class UserInfo(var name: String="", var age: Int=0, var email: String="",var genre: String="", var description: String="", var uid: String="")
