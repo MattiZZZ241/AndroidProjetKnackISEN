0fca33098e00c0f2949113e8e251885542224c9f
Matthis BERNET <matthis2401@gmail.com>
2023-01-31 11:26:46 +0100

    style + xml left

--- app/src/main/java/fr/isen/knackisen/androidprojet/fragment/LeftFragment.kt
+++ app/src/main/java/fr/isen/knackisen/androidprojet/fragment/LeftFragment.kt
@@ -1,21 +1,29 @@
 package fr.isen.knackisen.androidprojet.fragment
 
+import ListPostAdapter
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import fr.isen.knackisen.androidprojet.data.model.Comment
+import fr.isen.knackisen.androidprojet.data.model.Post
+import fr.isen.knackisen.androidprojet.data.model.Reactions
+import fr.isen.knackisen.androidprojet.data.model.User
 import fr.isen.knackisen.androidprojet.databinding.FragmentLeftBinding
 
 class LeftFragment : Fragment() {
     private var _binding: FragmentLeftBinding? = null
     private val binding get() = _binding!!
+    private lateinit var postContainer: List<Post>
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentLeftBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 }
